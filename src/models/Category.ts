88fb9b62074a5bee37aa6d4f107d34ed5d4615c6
Juliana Barros <dsbjuliana@gmail.com>
2021-05-14 16:20:23 -0300

    Desafio database upload

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
+
+import Transaction from './Transaction';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
